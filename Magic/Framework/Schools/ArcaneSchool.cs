86956ccea22fbda965445ce32356c083b138e63c
TyrianTyrell <55641224+TyrianTyrell@users.noreply.github.com>
2022-10-15 09:15:40 -0400

    Update ArcaneSchool.cs

--- Magic/Framework/Schools/ArcaneSchool.cs
+++ Magic/Framework/Schools/ArcaneSchool.cs
@@ -10,19 +10,10 @@ namespace Magic.Framework.Schools
         public ArcaneSchool()
             : base(SchoolId.Arcane) { }
 
-        public override Spell[] GetSpellsTier1()
-        {
-            return new[] { SpellManager.Get("arcane:analyze"), SpellManager.Get("arcane:magicmissle") };
-        }
+        public override Spell[] GetSpellsTier1() = new[] { SpellManager.Get("arcane:analyze"), SpellManager.Get("arcane:magicmissle") };
 
-        public override Spell[] GetSpellsTier2()
-        {
-            return new[] { SpellManager.Get("arcane:disenchant"), SpellManager.Get("arcane:enchant") };
-        }
+        public override Spell[] GetSpellsTier2() = new[] { SpellManager.Get("arcane:disenchant"), SpellManager.Get("arcane:enchant") };
 
-        public override Spell[] GetSpellsTier3()
-        {
-            return new[] { SpellManager.Get("arcane:rewind") };
-        }
+        public override Spell[] SpellsTier3 = new[] { SpellManager.Get("arcane:rewind") };
     }
 }
